efc0a4169ffedae17968dcb96714b0a9bb40f510
usertehrealol-stack <usertehrealol@gmail.com>
2026-01-23 21:41:39 +0000

    file not found
    ver 1.1

--- src/compiler.c
+++ src/compiler.c
@@ -8,6 +8,10 @@ int main() {
 	FILE *fptr;
 
 	fptr = fopen("main.a", "r"); // Edit main.a in the same folder you include this file.
+	if (fptr == NULL) { // if main.a was not found
+		printf("The main.a file was not found. Try putting main.a in the same directory/folder you put this compiler (well really interpreter) in and try again.\n");
+		return 1;
+	}
 	// Read file contents
 	char outPut[640]; // Realistically, what more do you need?
 	int number = 0; // For the counter
